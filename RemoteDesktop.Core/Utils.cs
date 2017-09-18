650d7517e9e85b85134e7a3df318fe2b28b0521b
Andrew <zezba9000@gmail.com>
2017-09-18 16:07:56 -0700

    optimized C method call

--- RemoteDesktop.Core/Utils.cs
+++ RemoteDesktop.Core/Utils.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Security;
 
 namespace RemoteDesktop.Core
 {
 	public static class Utils
 	{
+		[SuppressUnmanagedCodeSecurity]
 		[DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall)]
 		public static extern void RtlZeroMemory(IntPtr dst, IntPtr length);
 	}
