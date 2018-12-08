6eb71401b9670c39b0f67aa9ad615f18d96bcb19
Ryo Kanbayashi <ryo.contact@gmail.com>
2018-12-08 17:40:18 +0900

    add Xamarin.WPF project. but not build success yet

---
+++ RemoteDesktop.Client.Windows/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemoteDesktop.Client.Windows
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Xamarin.Forms.Platform.WPF.FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new RemoteDesktop.Client.Android.App());
+        }
+    }
+}
