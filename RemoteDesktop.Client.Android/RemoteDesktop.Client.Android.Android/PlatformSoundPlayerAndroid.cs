d1b866a48886c1721479b7769a9d6fb3142204f4
Ryo Kanbayashi <ryo.contact@gmail.com>
2018-12-17 09:07:00 +0900

    succeeded: bulding and running DependencyService used code succeeded at Android real device build. But debug operation was failed nazeka. tamatama kana

---
+++ RemoteDesktop.Client.Android/RemoteDesktop.Client.Android.Android/PlatformSoundPlayerAndroid.cs
@@ -0,0 +1,26 @@
+﻿using RemoteDesktop.Client.Android;
+using RemoteDesktop.Client.Android.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency (typeof (PlatformSoundPlayerAndroid))]
+
+namespace RemoteDesktop.Client.Android.Droid
+{
+        public class PlatformSoundPlayerAndroid: IPlatformSoundPlayer
+    {
+        public void PlayData(byte[] data, bool flag)
+        {
+
+        }
+
+        public bool Open(string waveOutDeviceName, int samplesPerSecond, int bitsPerSample, int channels, int bufferCount)
+        {
+            return true;
+        }
+
+        public void Close()
+        {
+
+        }
+    }
+}
