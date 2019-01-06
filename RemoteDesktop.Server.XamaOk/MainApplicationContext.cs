d2c3f94b66b3abb51d3b064c1422022d0400f277
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-01-07 08:22:37 +0900

    ffmpeg stream play with VLC OK (-r 1 -g 60 -vf format=rgb8)

--- RemoteDesktop.Server.XamaOk/MainApplicationContext.cs
+++ RemoteDesktop.Server.XamaOk/MainApplicationContext.cs
@@ -58,10 +58,11 @@ namespace RemoteDesktop.Server
         // using -f ssegment
         private string ffmpegForHLSArgs = "-y -loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 60 -vf format=yuv420p -f ssegment -segment_format mpegts -segment_list_type hls -segment_list " + outPathBase + "test.m3u8 -segment_time 1 -segment_list_size 4 -segment_list_flags +live -threads 4 -break_non_keyframes 1 " + outPathBase + "stream_%d.ts";
 
-        //private string ffmpegForDirectStreamingArgs = "-loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -r 1 -g 1 -vf format=yuv420p -maxrate 8192 -f mpegts tcp://192.168.1.8:8888?listen";
         // -crf (0-51)でクオリティ設定
-        private string ffmpegForDirectStreamingArgs = "-loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 60 -vf format=yuv420p -f mpegts tcp://192.168.0.11:8888?listen";
-        //private string ffmpegForDirectStreamingArgs = "-loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 60 -vf format=rgb8 -f mpegts tcp://192.168.0.11:8888?listen";
+        //private string ffmpegForDirectStreamingArgs = "-loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 60 -vf format=yuv420p -f mpegts tcp://192.168.0.11:8888?listen";
+
+        private string ffmpegForDirectStreamingArgs = "-loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 60 -vf format=rgb8 -f mpegts tcp://192.168.0.11:8888?listen";
+
 
         public MainApplicationContext()
 		{
