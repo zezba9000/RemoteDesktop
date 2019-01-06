c72283e635c4f82ceebf989508838b28f02f8088
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-01-07 07:44:45 +0900

    HLS that 1file per frame is succeeded. but can not play the stream with my player of Android and VLC (2)

--- RemoteDesktop.Server.XamaOk/MainApplicationContext.cs
+++ RemoteDesktop.Server.XamaOk/MainApplicationContext.cs
@@ -57,9 +57,11 @@ namespace RemoteDesktop.Server
         //private string ffmpegForHLSArgs = "-y -f image2pipe -framerate 1 -i - -c:v libx264 -r 1 -vf format=yuv420p -f hls -r 1 -g 10 -hls_time 1 -hls_list_size 3 -hls_allow_cache 0 -hls_segment_filename " + outPathBase + "stream_single.ts -hls_flags delete_segments -hls_flags single_file " + outPathBase + "test.m3u8";
 
         //private string ffmpegForHLSArgs = "-y -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 60 -vf format=yuv420p -f hls -tune zerolatency -r 1 -g 60 -hls_time 1 -hls_list_size 4 -hls_allow_cache 0 -hls_segment_filename " + outPathBase + "stream_%d.ts -hls_flags delete_segments " + outPathBase + "test.m3u8";
-        private string ffmpegForHLSArgs = "-y -loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -tune zerolatency -r 1 -g 1 -vf format=yuv420p -f hls -r 1 -g 1  -hls_time 1 -hls_list_size 4 -hls_allow_cache 0 -hls_segment_filename " + outPathBase + "stream_%d.ts -hls_flags delete_segments " + outPathBase + "test.m3u8";
-        
-        //private string ffmpegForHLSArgs = "-y -loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 1 -vf format=yuv420p -f ssegment -segment_format mpegts -segment_list " + outPathBase + "test.m3u8 -segment_time 1 -segment_list_type hls -segment_list_size 4 -segment_list_flags +live -threads 4 -break_non_keyframes 1" + outPathBase + "stream_%d.ts";
+
+        //private string ffmpegForHLSArgs = "-y -loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -tune zerolatency -r 1 -g 60 -vf format=yuv420p -f hls -r 1 -g 60  -hls_time 1 -hls_list_size 4 -hls_allow_cache 0 -hls_segment_filename " + outPathBase + "stream_%d.ts -hls_flags delete_segments " + outPathBase + "test.m3u8";
+
+        private string ffmpegForHLSArgs = "-y -loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 60 -vf format=yuv420p -f ssegment -segment_format mpegts -segment_list_type hls -segment_list " + outPathBase + "test.m3u8 -segment_time 1 -segment_list_size 4 -segment_list_flags +live -threads 4 -break_non_keyframes 1 " + outPathBase + "stream_%d.ts";
+
         //private string ffmpegForDirectStreamingArgs = "-loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -r 1 -g 1 -vf format=yuv420p -maxrate 8192 -f mpegts tcp://192.168.1.8:8888?listen";
         // -crf (0-51)でクオリティ設定
         private string ffmpegForDirectStreamingArgs = "-loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 60 -vf format=yuv420p -f mpegts tcp://192.168.0.11:8888?listen";
@@ -134,8 +136,8 @@ namespace RemoteDesktop.Server
             startInfo.CreateNoWindow = true;
             startInfo.FileName = ffmpegPath;
 
-            ///startInfo.Arguments = ffmpegForHLSArgs;
-            startInfo.Arguments = ffmpegForDirectStreamingArgs;
+            startInfo.Arguments = ffmpegForHLSArgs;
+            //startInfo.Arguments = ffmpegForDirectStreamingArgs;
 
             ffmpegProc = new Process();
             ffmpegProc.StartInfo = startInfo;
