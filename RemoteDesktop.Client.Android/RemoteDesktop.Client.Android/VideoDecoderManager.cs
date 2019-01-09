8877f2100570955286b905124e901dfcc43571f8
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-01-10 07:39:50 +0900

    implmented Decoder class for Xamarin.Forms

---
+++ RemoteDesktop.Client.Android/RemoteDesktop.Client.Android/VideoDecoderManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoteDesktop.Client.Android
+{
+    class VideoDecoderManager
+    {
+        public bool Opened = false;
+        IPlatformVideoDecoder pdecoder;
+
+        public VideoDecoderManager()
+        {
+            pdecoder = VideoDecoderFactory.getInstance();
+        }
+
+        //public void PlayData(byte[] data, bool flag)
+        //{
+        //    pplayer.PlayData(data, flag);
+        //}
+
+        //public bool Open(string waveOutDeviceName, int samplesPerSecond, int bitsPerSample, int channels, int bufferCount)
+        //{
+        //    pplayer.Open(waveOutDeviceName, samplesPerSecond, bitsPerSample, channels, bufferCount);
+        //    Opened = true;
+        //    return true;
+        //}
+
+        public bool setup()
+        {
+            return pdecoder.setup();
+        }
+
+        public byte[] getDecodedFrame()
+        {
+            return pdecoder.getDecodedFrame();
+        }
+
+        void addEncodedFrame(byte[] frame_data)
+        {
+            pdecoder.addEncodedFrame(frame_data);
+        }
+
+        public void Close()
+        {
+            pdecoder.Close();
+            Opened = false;
+        }
+    }
+}
