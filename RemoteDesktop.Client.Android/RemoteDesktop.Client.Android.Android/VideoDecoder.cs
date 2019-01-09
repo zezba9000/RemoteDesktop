686cef15414431b8e15bf060f7bd9b43d216ceeb
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-01-09 23:54:05 +0900

    start implemnt encoder class for Android

---
+++ RemoteDesktop.Client.Android/RemoteDesktop.Client.Android.Android/VideoDecoder.cs
@@ -0,0 +1,182 @@
+﻿using Android.Media;
+using RemoteDesktop.Client.Android;
+using RemoteDesktop.Client.Android.Droid;
+using System;
+using System.Threading;
+using Xamarin.Forms;
+
+//using java.io.IOException;
+//using java.nio.ByteBuffer;
+
+//using Android.Media.MediaCodec.BufferInfo;
+//using Android.Media.MediaExtractor;
+//using Android.Media.MediaFormat;
+
+using Android.Views;
+using Android.Graphics;
+
+//[assembly: Dependency(typeof(PlatformSoundPlayerAndroid))]
+
+namespace RemoteDesktop.Client.Android.Droid
+{
+    public class VideoDecoder// : IPlatformSoundPlayer
+    {
+        private static String MIME = "video/avc";
+        private static String TAG = "VideoDecoder";
+//	    private MediaExtractor mExtractor;
+        private MediaCodec mDecoder;
+
+        private bool eosReceived;
+        private Java.Nio.ByteBuffer[] inputBuffers = null;
+        
+		
+        private bool isInput = true;
+        private bool first = false;
+        private long startWhen = 0;
+        private MediaCodec.BufferInfo info = null;
+
+        private long CurrentTimeMillisSharp()
+        {
+            return (long) (new TimeSpan(DateTime.UtcNow.Ticks).TotalMilliseconds);
+        }
+
+        //public boolean init(Surface surface, String filePath)
+        public bool setup()
+        {
+            eosReceived = false;
+            try
+            {
+                //mExtractor = new MediaExtractor();
+                //mExtractor.setDataSource(filePath);
+
+                //for (int i = 0; i < mExtractor.getTrackCount(); i++)
+                //{
+                //    MediaFormat format = mExtractor.getTrackFormat(i);
+
+                //    String mime = format.getString(MediaFormat.KEY_MIME);
+                //    if (mime.startsWith(VIDEO))
+                //    {
+                //        mExtractor.selectTrack(i);
+                        mDecoder = MediaCodec.CreateDecoderByType(MIME);
+                        try
+                        {
+                    //mDecoder.configure(format, surface, null, 0 /* Decoder */);
+                    MediaFormat format = new MediaFormat();
+                    format.SetString(MediaFormat.KeyMime, MIME);
+                    mDecoder.Configure(format, new Surface(new SurfaceTexture(true)), null, 0);
+
+                }
+                        catch (Exception ex)
+                        {
+                            return false;
+                        }
+
+                        mDecoder.Start();
+                //        break;
+                //    }
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            info = new MediaCodec.BufferInfo();
+            inputBuffers = mDecoder.GetInputBuffers();
+            mDecoder.GetOutputBuffers();
+
+            return true;
+        }
+
+        // if eosReceived == false, return false
+        public bool checkStatus()
+        {
+
+            //ByteBuffer[] inputBuffers = mDecoder.getInputBuffers();
+            //mDecoder.getOutputBuffers();
+
+            //boolean isInput = true;
+            //boolean first = false;
+            //long startWhen = 0;
+
+            if (isInput)
+            {
+                int inputIndex = mDecoder.DequeueInputBuffer(10000);
+                if (inputIndex >= 0)
+                {
+                    // fill inputBuffers[inputBufferIndex] with valid data
+                    Java.Nio.ByteBuffer inputBuffer = inputBuffers[inputIndex];
+
+                    int sampleSize = mExtractor.ReadSampleData(inputBuffer, 0);
+
+                    if (mExtractor.Advance() && sampleSize > 0)
+                    {
+                        mDecoder.QueueInputBuffer(inputIndex, 0, sampleSize, mExtractor.getSampleTime(), 0);
+
+                    }
+                    else
+                    {
+                        mDecoder.QueueInputBuffer(inputIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+                        isInput = false;
+                    }
+                }
+            }
+
+            int outIndex = mDecoder.DequeueOutputBuffer(info, 10000);
+            switch (outIndex)
+            {
+                case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED:
+                    mDecoder.GetOutputBuffers();
+                    break;
+
+                case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:
+                    break;
+
+                case MediaCodec.INFO_TRY_AGAIN_LATER:
+                    //				Log.d(TAG, "INFO_TRY_AGAIN_LATER");
+                    break;
+
+                default:
+                    if (!first)
+                    {
+                        startWhen = CurrentTimeMillisSharp();
+                        first = true;
+                    }
+                    try
+                    {
+                        int sleepTime = (int)((info.PresentationTimeUs / 1000) - (CurrentTimeMillisSharp() - startWhen);
+
+                        if (sleepTime > 0)
+                            Thread.Sleep(sleepTime);
+                    }
+                    catch (Exception ex)
+                    {
+                        // TODO Auto-generated catch block
+                        Console.WriteLine(ex);
+                    }
+
+                    mDecoder.ReleaseOutputBuffer(outIndex, true /* Surface init */);
+                    //break;
+                    return false;
+            }
+
+            // All decoded frames have been rendered, we can stop playing now
+            if ((info.Flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0)
+            {
+                //break;
+                return false;
+            }
+
+            return eosReceived == true;
+        }
+
+        public void close()
+        {
+            mDecoder.Stop();
+            mDecoder.Release();
+//            mExtractor.Release();
+            eosReceived = true;
+        }
+    }
+}
