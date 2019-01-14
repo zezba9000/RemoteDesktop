ce4015d79b719bb2f9d3bd905287de7c678ad392
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-01-14 14:50:12 +0900

    Try stream local mp4 file is not succeeded...

--- RemoteDesktop.Server.XamaOk/MainApplicationContext.cs
+++ RemoteDesktop.Server.XamaOk/MainApplicationContext.cs
@@ -72,7 +72,10 @@ namespace RemoteDesktop.Server
         //private string ffmpegForHLSArgs = "-y -loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 2  -bsf:v h264_mp4toannexb -map 0 -f rtp -sdp_file " + outPathBase +  "saved_sdp_file.sdp rtp://192.168.0.11:8888";
 
         // RTSP
-        private string ffmpegForHLSArgs = "-y -loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 30  -bsf:v h264_mp4toannexb -map 0 -f rtsp -rtsp_transport tcp  -rtsp_flags listen rtsp://" + RTPConfiguration.ServerAddress + ":8889/live.sdp";
+        //private string ffmpegForHLSArgs = "-y -loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 30  -bsf:v h264_mp4toannexb -map 0 -f rtsp -rtsp_transport tcp  -rtsp_flags listen rtsp://" + RTPConfiguration.ServerAddress + ":8889/live.sdp";
+
+        // RTSP send to my C# server
+        private string ffmpegForHLSArgs = "-y -loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 30  -bsf:v h264_mp4toannexb -map 0 -f rtsp -rtsp_transport tcp rtsp://" + RTPConfiguration.ServerAddress + ":" + (RTPConfiguration.ImageServerPort + 1).ToString() + "/";
 
         // -crf (0-51)でクオリティ設定
         //private string ffmpegForDirectStreamingArgs = "-loglevel debug -f image2pipe -framerate 1 -i - -c:v libx264 -preset veryslow -tune zerolatency -r 1 -g 60 -vf format=yuv420p -f mpegts tcp://192.168.0.11:8888?listen";
@@ -111,10 +114,8 @@ namespace RemoteDesktop.Server
             //while ((line = input.ReadLine()) != null) { }
             //input.Dispose();
 
-
             if (RTPConfiguration.isUseFFMPEG)
             {
-
                 kickFFMPEG();
                 timer = new System.Windows.Forms.Timer();
                 timer.Interval = (int) (1000 * (1.0f / (float)targetFPS));
@@ -122,7 +123,7 @@ namespace RemoteDesktop.Server
                 timer.Start();
                 //encoder = new ExtractedH264Encoder(540, 960, 20 * 1024 * 8 , 1.0f, 10.0f);
                 //encoder.aviDataGenerated += h264AVIDataHandlerHlsFFMPEG;
-            }
+            } 
 
             if (RTPConfiguration.isUseRTSPLib)
             {
@@ -134,19 +135,20 @@ namespace RemoteDesktop.Server
                 //rtsp_serv.TryAddMedia(new Media.Rtsp.Server.MediaTypes.RtspSource() // ffmpegに接続？ffmpegからこっちに流させる？
                 //rtsp_serv.TryAddMedia(new Media.Rtsp.Server.MediaTypes.RtspSource("test", "rtsp://" + RTPConfiguration.ServerAddress + ":" + RTPConfiguration.ImageServerPort.ToString() + "/live.sdp"));
                 //rtsp_serv.TryAddMedia(new Media.Rtsp.Server.MediaTypes.RtspSource("test", "rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4"));
-                rtsp_serv.TryAddMedia(new Media.Rtsp.Server.MediaTypes.RtspSource("test", "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"));
+
+                //rtsp_serv.TryAddMedia(new Media.Rtsp.Server.MediaTypes.RtspSource("test", "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"));
+
+                var h264_src = new Media.Rtsp.Server.MediaTypes.RFC6184Media(540,960,"test");
+                h264_src.Source = new System.Uri("file://work/tmp/gen_HLS_files_from_h264_avi_file_try/capturedDatax2.mp4");
+                rtsp_serv.TryAddMedia(h264_src);
 
                 // can be accessed rtsp://sever_addr:port/test
                 rtsp_serv.Start();
 
-                var streams = rtsp_serv.MediaStreams;
-                //Media.Rtsp.RtspServer.RtspRequestHandler = rtspReuestHandlerTest;
-                //Media.Rtsp.RtspServer.RtspRequestHandler handler = new Media.Rtsp.RtspServer.RtspRequestHandler(rtspReuestHandlerTest);
-                rtsp_serv.TryAddRequestHandler(Media.Rtsp.RtspMethod.DESCRIBE, rtspReuestHandlerTest);
-                Console.WriteLine(streams);
-            }
-
-            
+                //var streams = rtsp_serv.MediaStreams;
+                //rtsp_serv.TryAddRequestHandler(Media.Rtsp.RtspMethod.DESCRIBE, rtspReuestHandlerTest);
+                //Console.WriteLine(streams);
+            }           
 
             //var rtspsrc = new Media.Rtsp.Server. RtspSource();
 
@@ -164,7 +166,7 @@ namespace RemoteDesktop.Server
         private bool rtspReuestHandlerTest(RtspMessage request, out RtspMessage response)
         {
             Console.WriteLine(request);
-            response = request;
+            response = request; // 苦肉の策
             return true;
         }
 
