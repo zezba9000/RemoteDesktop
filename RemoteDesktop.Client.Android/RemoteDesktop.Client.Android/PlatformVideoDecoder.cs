3eecf57b1afbf11d56f80a3e68971c1f927b9f5e
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-01-15 18:52:50 +0900

    add Android VideoDecoder cs

---
+++ RemoteDesktop.Client.Android/RemoteDesktop.Client.Android/PlatformVideoDecoder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RemoteDesktop.Client.Android
+{
+
+    public interface IPlatformVideoDecoder
+    {
+        bool setup(byte[] format_hint);
+        byte[] getDecodedFrame();
+        void addEncodedFrame(byte[] frame_data);
+        void Close();
+    }
+
+    public static class VideoDecoderFactory
+    {
+        public static IPlatformVideoDecoder getInstance()
+        {
+            return DependencyService.Get<IPlatformVideoDecoder>();
+        }
+    }
+}
