4b45964695f9bbd43491bce4bf3da708b4350733
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-01-24 09:23:29 +0900

    fixedResolutionScale 0.25 => crash 0.2 => works!!!. encoder may have encodable size ranges...

--- RemoteDesktop.Server.XamaOk/MainApplicationContext.cs
+++ RemoteDesktop.Server.XamaOk/MainApplicationContext.cs
@@ -35,7 +35,7 @@ namespace RemoteDesktop.Server
         bool isFixedParamUse = true; // use server side hard coded parameter on running
         bool fixedCompress = false;
         float resolutionScale = 1.0F; // DEBUG INFO: current jpeg encoding implementation is not work with not value 1.0
-        float fixedResolutionScale = 0.5F;
+        float fixedResolutionScale = 0.2F;
 		private System.Windows.Forms.Timer timer = null;
 		public static Dispatcher dispatcher;
 
