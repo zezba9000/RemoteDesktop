c08f494106401afc572b316a87348eab95d08185
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-02-02 09:22:22 +0900

    use csd-0 from AAC stream head 2byte. pass it decoder as first frame but not work...

--- RemoteDesktop.Client.Android/RemoteDesktop.Client.Android.Android/PlatformAudioDecodingPlayerAndroid.cs
+++ RemoteDesktop.Client.Android/RemoteDesktop.Client.Android.Android/PlatformAudioDecodingPlayerAndroid.cs
@@ -57,19 +57,19 @@ namespace RemoteDesktop.Client.Android.Droid
                 if (sampleSize > 0)
                 {
                     ByteBuffer inputBuffer = mDecoder.GetInputBuffer(inputBufferId);
-                    //inputBuffer.Position(0);
+                    inputBuffer.Position(0);
 
                     Console.WriteLine("QueueInputBuffer inputIndex=" + inputBufferId.ToString());
-                    //if (frameCounter == 0)
-                    //{
-                    //    inputBuffer.Put(CSD0);
-                    //    mDecoder.QueueInputBuffer(inputBufferId, 0, sampleSize, 0, MediaCodec.BufferFlagCodecConfig);
-                    //}
-                    //else
-                    //{
+                    if (frameCounter == 0)
+                    {
+                        inputBuffer.Put(CSD0);
+                        mDecoder.QueueInputBuffer(inputBufferId, 0, sampleSize, 0, MediaCodec.BufferFlagCodecConfig);
+                    }
+                    else
+                    {
                         inputBuffer.Put(encoded_data);
                         mDecoder.QueueInputBuffer(inputBufferId, 0, sampleSize, 0, 0);
-                    //}
+                    }
                     frameCounter++;
                 }
                 else
@@ -217,8 +217,8 @@ namespace RemoteDesktop.Client.Android.Droid
             mDecoder = MediaCodec.CreateDecoderByType("audio/mp4a-latm");
             var mMediaFormat = MediaFormat.CreateAudioFormat("audio/mp4a-latm", samplingRate, ch);
             //byte[] bytes = new byte[] { (byte)0x12, (byte)0x12 };
-            ByteBuffer bb = ByteBuffer.Wrap(csd0_data);
-            mMediaFormat.SetByteBuffer("csd-0", bb);
+            //ByteBuffer bb = ByteBuffer.Wrap(csd0_data);
+            //mMediaFormat.SetByteBuffer("csd-0", bb);
             var cbk = new AudioDecoderCallback(mDecoder, mCallbackObj, this);
             cbk.CSD0 = csd0_data;
             mDecoder.SetCallback(cbk, handler);
