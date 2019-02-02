e0068fe9e64ef8e811aa2990789219d2bea42f62
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-02-02 19:45:19 +0900

    tuned: 8000Hz, 16bit depth is played. not noisy but totyu de okasiku naru no wa kawarazu.

--- RemoteDesktop.Client.Android/RemoteDesktop.Client.Android.Android/PlatformAudioDecodingPlayerAndroid.cs
+++ RemoteDesktop.Client.Android/RemoteDesktop.Client.Android.Android/PlatformAudioDecodingPlayerAndroid.cs
@@ -213,7 +213,7 @@ namespace RemoteDesktop.Client.Android.Droid
 
         public bool setup(AudioDecodingPlayerCallback callback_obj, int samplingRate, int ch, int bitrate, byte[] csd_data)
         {
-            OpenDevice("hoge", samplingRate, 8, ch, 128 * 1024);
+            OpenDevice("hoge", samplingRate, 16, ch, 128 * 1024);
 
             mCallbackObj = callback_obj;
             HandlerThread callbackThread = new HandlerThread("AACDecodingPlayerHandler");
